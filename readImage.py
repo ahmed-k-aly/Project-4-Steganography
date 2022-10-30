f370994091709422b7a15df06a6e2085ed631f3d
ngacho <brandonngacho@gmail.com>
2022-10-29 20:30:12 -0400

    minor bug fixes and some improvements

--- readImage.py
+++ readImage.py
@@ -1,6 +1,6 @@
 import binascii
 import numpy as np
-import imageio
+import imageio.v2 as imageio 
 
 
 def main():
@@ -10,15 +10,24 @@ def main():
     chars = getLSBchannels(img, height, width)
     #assert(len(chars) == 32 * 3)
     # convert binary to ASCII
-    chars = binaryToASCII(chars)
-    print(chars)
+    chars = decode_binary_string(''.join(chars))
+    writeToFile(chars, "message.txt")
 
 def binaryToASCII(chars):
     # convert binary to ASCII
     # https://stackoverflow.com/questions/7396849/convert-binary-to-ascii-and-vice-versa
-    n = int(''.join(chars), 2)
-    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode()
+    n = binaryToInt(''.join(chars))
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode() # buggy code
 
+
+def decode_binary_string(s):
+    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
+
+    """
+        Given a binary string, return an int
+    """
+def binaryToInt(binaryString):
+    return int(binaryString, 2)
 
 
     """
@@ -29,13 +38,20 @@ def getLSBchannels(img, height, width):
     count = 0
     for r in range(height):
         for c in range(width):
-            if count < 32:
+            if count < 1525: # string ends at char 1525 i think.
                 chars.append(str(img[r,c,0] & 1)) # get LSB of first channel
                 chars.append(str(img[r,c,1] & 1)) # get LSB of second channel
                 chars.append(str(img[r,c,2] & 1)) # get LSB of third channel
                 count += 1
-    return chars
-    
+    # return string instead of array.
+    return ''.join(chars)
+
+    """
+        Write the output to file.
+    """
+def writeToFile(text, file_name):
+    with open(file_name, 'w') as f:
+        f.write(text)
 
 if __name__ == "__main__":
     main()
